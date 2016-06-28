18f2cdd4cd66414c2c3a7da1deab7e57d504c0c7
Eric J. Ma <ericmajinglong@gmail.com>
2016-06-27 21:41:10 -0400

    initial commit of sequence transformer test

---
+++ notebooks/test_sequence_transformer.py
@@ -0,0 +1,22 @@
+from sequence_transformer import to_numeric_rep, standardize_sequence
+
+new_sequence1 = 'PQITLNQRTLVTIPIGGDLKEALLKTGADDTVLEEMNLPGRMKPKMIGGIGGFIKVRQYD'\
+    'QILIEICGRKAIGTVLVGPTPVNIIGRNLLTQVGCTLNFP'
+
+new_sequence2 = 'PQITLWQRPLVTIKIGGQLKEALLKTGADDTVLEEMNLPGRWKPKMIRGIGGFIKMRQYD'\
+    'KILIEICGHQAIGTVLVGPTPRNIIGDNLLTIIGCTLN'
+
+arr1 = to_numeric_rep(new_sequence1)
+arr2 = to_numeric_rep(new_sequence2)
+
+std1 = standardize_sequence(arr1, 'protease')
+std2 = standardize_sequence(arr2, 'protease')
+
+
+def test_to_numeric_rep():
+    assert arr1.size == len(arr1)
+    assert arr2.size == len(arr2)
+
+
+def test_standardize_sequence():
+    assert std1.shape == std2.shape
