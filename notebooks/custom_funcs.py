a006e697d09a20cfe40b969c820516f193991af6
Eric J. Ma <ericmajinglong@gmail.com>
2016-07-07 15:04:01 -0400

    multiple changes, will not describe.

--- notebooks/custom_funcs.py
+++ notebooks/custom_funcs.py
@@ -9,23 +9,44 @@ allowed_drugnames = ['FPV', 'ATV', 'IDV', 'LPV', 'NFV', 'SQV', 'TPV', 'DRV',
                      '3TC', 'ABC', 'AZT', 'D4T', 'DDI', 'TDF', 'EFV', 'NVP',
                      'ETR', 'RPV',
                      ]
+drug_col_vals = {'protease': 8,
+                 'nnrt': 4,
+                 'nrt': 6}
 
 
-def read_data(protein):
+def read_data(protein, sparse=True):
     """
     Reads in the data for the protein.
-    """
-    drug_col_vals = {'protease': 8,
-                     'nnrt': 4,
-                     'nrt': 6}
 
+    Has two options:
+    - sparse=True: loads the version that has dashes replacing consensus
+                   sequence.
+    - sparse=False: loads version has actual letters in each position from
+                    consensus sequence.
+
+    Returns:
+    ========
+    - data: (pd.DataFrame) protein sequence matched with drug resistance data
+            (for all drugs)
+    - drug_cols: (iterable) list of drug columns names.
+    - feat_cols: (iterable) list of feature column names (corresponding to
+                 sequence position)
+    """
     assert protein in drug_col_vals.keys()
 
-    data = pd.read_csv('../data/hiv-{0}-data.csv'.format(protein),
-                       index_col='SeqID')
+    if sparse:
+        path = '../data/hiv-{0}-data-sparse.csv'.format(protein)
+        sep = '\t'
+    else:
+        path = '../data/hiv-{0}-data.csv'.format(protein)
+        sep = ','
+
+    data = pd.read_csv(path, index_col='SeqID', sep=sep)
 
     drug_cols = data.columns[0:drug_col_vals[protein]]
     feat_cols = data.columns[drug_col_vals[protein]:]
+    for col in feat_cols:
+        data[col] = data[col].str.upper()
 
     return data, drug_cols, feat_cols
 
@@ -52,9 +73,17 @@ def replace_ambiguous_letters_with_nan(df, feat_cols):
     """
     assert isinstance(df, pd.DataFrame)
 
+    def is_allowed(x):
+        if x not in ['#', '~']:
+            return True
+        else:
+            return False
+
     new_df = df.copy()
     for col in feat_cols:
         new_df[col] = df[col].apply(lambda x: np.nan if len(str(x)) > 1 else x)
+        new_df[col] = new_df[col].apply(lambda x: np.nan
+                                        if not is_allowed(x) else x)
     return new_df
 
 
@@ -96,19 +125,18 @@ def replace_dashes_with_canonical_letters(df, consensus_map, feat_cols):
     return df
 
 
-def read_consensus(protein_name):
+def read_consensus(drug_class):
     """
     Reads in the consensus sequence, makes a map of position to letter.
     """
 
     # Defensive programming checks.
-    allowed_names = ['protease', 'nnrt', 'nrt']
-    assert protein_name in allowed_names,\
-        "protein_name must be in {0}".format(allowed_names)
+    assert drug_class in drug_col_vals.keys(),\
+        "drug_class must be in {0}".format(drug_col_vals.keys())
 
-    if protein_name == 'protease':
+    if drug_class == 'protease':
         handle = '../data/hiv-protease-consensus.fasta'
-    elif protein_name in ['nnrt', 'nrt']:
+    elif drug_class in ['nnrt', 'nrt']:
         handle = '../data/hiv-rt-consensus.fasta'
     consensus = SeqIO.read(handle, 'fasta')
     consensus_map = {i: letter for i, letter in enumerate(str(consensus.seq))}
@@ -130,18 +158,43 @@ def drop_na_from_data(df, drug_name, feat_cols):
     return df[columns].dropna()
 
 
-def get_cleaned_data(protein_name, drug_name):
+def get_protein_drug_data(drug_class, sparse=False):
     """
-    A composition of the above functions. Expresses the "business logic"
-    behind the data preprocessing steps.
+    A composition of the above functions.
+
+    Returns a dataframe that may contain NaN cells. Contains all of the drug
+    resistance data (floating point columns) matched with the sequences. Drug
+    resistance columns are not log10 transformed. Also returns the drug
+    columns and feature columns.
     """
-    data, drug_cols, feat_cols = read_data(protein_name)
-    consensus_map = read_consensus(protein_name)
+    data, drug_cols, feat_cols = read_data(drug_class, sparse=sparse)
+    consensus_map = read_consensus(drug_class)
     data = replace_ambiguous_letters_with_nan(data, feat_cols)
     data = replace_dashes_with_canonical_letters(data,
                                                  consensus_map,
                                                  feat_cols)
 
+    return data, drug_cols, feat_cols
+
+
+def sparse_to_dense_data(drug_class):
+    """
+    A function that converts the sparsely-represented data to a dense form.
+    """
+    data, drug_cols, feat_cols = get_protein_drug_data(drug_class, sparse=True)
+    data.to_csv('../data/hiv-{0}-data.csv'.format(drug_class))
+
+
+def get_cleaned_data(drug_class, drug_name):
+    """
+    A composition of the above functions. Expresses the "business logic"
+    behind the data preprocessing steps.
+
+    Returns a clean dataframe with no NaN cells. Positions 1 to (n-1) are the
+    amino acids, while the last position is a single column of floating point
+    numbers.
+    """
+    data, drug_cols, feat_cols = get_protein_drug_data(drug_class)
     data = drop_na_from_data(data, drug_name, feat_cols)
     data[drug_name] = data[drug_name].apply(lambda x: np.log10(x))
 
@@ -149,15 +202,19 @@ def get_cleaned_data(protein_name, drug_name):
 
 
 def to_numeric_rep(df, feat_cols, rep='mw'):
+    df_new = df.copy()
+    allowed_rep = ['mw', 'pKa']
+    assert rep in allowed_rep, 'kwarg rep must be in {0}'.format(allowed_rep)
+
     if rep == 'mw':
         numeric_dict = molecular_weights
     if rep == 'pKa':
         numeric_dict = isoelectric_points
     for col in feat_cols:
-        df[col] = df[col].replace(numeric_dict.keys(),
-                                  numeric_dict.values())
+        df_new[col] = df_new[col].replace(numeric_dict.keys(),
+                                          numeric_dict.values())
 
-    return df
+    return df_new
 
 
 def test_data_integrity(data):
