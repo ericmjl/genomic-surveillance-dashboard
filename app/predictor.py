9412b1668e58b53cb5c1bc231e2d3c65d5754311
Eric J. Ma <ericmajinglong@gmail.com>
2016-08-04 09:18:21 -0400

    first attempt at adding prediction with intervals

--- app/predictor.py
+++ app/predictor.py
@@ -9,10 +9,15 @@ The front-end interface looks something like this:
 from flask import Flask, render_template, request
 from gsdash.sequence_transformer import to_numeric_rep, standardize_sequence
 from sklearn.externals import joblib
-from bokeh.charts import Bar
+from bokeh.charts import Bar, BoxPlot
 from bokeh.resources import INLINE
 from bokeh.embed import components
 from bokeh.models import HoverTool, ResetTool
+from bokeh.plotting import figure
+
+import numpy as np
+
+
 drugs = ['FPV', 'ATV', 'IDV', 'LPV', 'NFV', 'SQV', 'TPV', 'DRV',]
 
 predictor = Flask(__name__)
@@ -23,6 +28,46 @@ predictor = Flask(__name__)
 def home():
     return render_template('predictor/index.html')
 
+def pred_range(model, datum):
+    preds = np.zeros(len(model.estimators_))
+    for i, est in enumerate(model.estimators_):
+        preds[i] = est.predict(datum).reshape(1, -1)[0]
+    return preds
+
+def pred_intervals(model, datum, percentile=95):
+    preds = pred_range(model, datum)
+    mean = np.mean(preds)
+    lowbo = np.percentile(preds, (100 - percentile) / 2. )
+    uppbo = np.percentile(preds, 100 - (100 - percentile) / 2.)
+
+    return mean, lowbo, uppbo
+
+def predict_withrange(drugs, seq):
+    all_predictions = list()
+    for drug in drugs:
+        print(drug)
+        model = joblib.load("../models/base/{drug}/{drug}.pkl".format(drug=drug))
+        data = dict()
+        data['drug'] = drug
+        data['log10(DR)'] = pred_range(model, seq)
+        print(data)
+    all_predictions.append(data)
+    return all_predictions
+
+def predict_norange(drugs, seq):
+    preds = dict()
+    preds['drug'] = list()
+    preds['log10(DR)'] = list()
+
+    for drug in drugs:
+        # print(drug)
+        mdl = joblib.load("../models/base/{drug}/{drug}.pkl".format(drug=drug))
+        pred = mdl.predict(seq)[0]
+        # preds.append(dict(drug=drug, pred=pred))
+        preds['drug'].append(drug)
+        preds['log10(DR)'].append(pred)
+        # print(pred)
+    return preds
 
 @predictor.route('/predict', methods=['POST'])
 def predict():
@@ -30,18 +75,8 @@ def predict():
     seq = standardize_sequence(to_numeric_rep(input_sequence, 'mw'), 'protease')
     seq = seq.reshape(1, -1)
 
-    preds = dict()
-    preds['drug'] = list()
-    preds['log10(DR)'] = list()
-
-    for drug in drugs:
-        print(drug)
-        mdl = joblib.load("../models/base/{drug}/{drug}.pkl".format(drug=drug))
-        pred = mdl.predict(seq)[0]
-        # preds.append(dict(drug=drug, pred=pred))
-        preds['drug'].append(drug)
-        preds['log10(DR)'].append(pred)
-        print(pred)
+    # preds = predict_norange(drugs, seq)
+    preds = predict_withrange(drugs, seq)
 
     hover = HoverTool()
     hover.tooltips = [
@@ -53,6 +88,8 @@ def predict():
 
     bar = Bar(data=preds, values='log10(DR)', label='drug', title="protease drug resistance",
               plot_width=600, plot_height=400, legend=False, tools=TOOLS)
+    # bar = BoxPlot(data=preds, values='log10(DR)', label='drug', title="protease drug resistance",
+    #               plot_width=600, plot_height=400, legend=False, tools=TOOLS)
     # for r in bar.renderers:
         # print(r.data_source.data)
     # print(bar.renderers)
